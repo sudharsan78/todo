5afc302e36be4c4c68487d5b410747417df4109b
sudharsan78 <sudharsan@testpress.in>
2016-04-13 11:53:06 +0530

    Create api snippet app

--- todolist/models.py
+++ todolist/models.py
@@ -7,7 +7,7 @@ from rest_framework import serializers
 class Todo(models.Model):
     task = models.CharField(max_length=100, unique=True)
     def __str__(self):
-        return self.work_list
+        return self.task
 	class Meta:
 	    ordering = ['task']
 				
