c2e9019315625d705c8f3512eba3aab75163b0b4
sudharsan78 <sudharsan@testpress.in>
2016-04-16 13:31:41 +0530

    Remove duplicate model and change url

--- todolist/urls.py
+++ todolist/urls.py
@@ -4,12 +4,12 @@ from . import views
 
 app_name='todolist'
 urlpatterns=[
-    #url(r'^$',views.index, name='index'),
-    #url(r'^add_work/$',views.add_work, name='add_work'),
-    #url(r'^(?P<pk>\d+)/work_done/$',views.work_done, name='work_done'),
-    #url(r'^(?P<pk>\d+)/not_done/$',views.not_done, name='not_done'),
-    #url(r'^(?P<pk>\d+)/remove_work/$',views.remove_work, name='remove_work'),
-    url(r'^$', views.TodoAPIView.as_view()),
+    url(r'^$',views.index, name='index'),
+    url(r'^add_work/$',views.add_work, name='add_work'),
+    url(r'^(?P<pk>\d+)/work_done/$',views.work_done, name='work_done'),
+    url(r'^(?P<pk>\d+)/not_done/$',views.not_done, name='not_done'),
+    url(r'^(?P<pk>\d+)/remove_work/$',views.remove_work, name='remove_work'),
+    url(r'^todoapi/v1/$', views.TodoAPIView.as_view()),
       
 ]
 urlpatterns = format_suffix_patterns(urlpatterns)
