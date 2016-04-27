c433249fd109585f85cd3ab96ec165f7838dc02f
sudharsan78 <sudharsan@testpress.in>
2016-04-27 19:21:15 +0530

    Remove dj_database_url from settings

--- todo/settings/production.py
+++ todo/settings/production.py
@@ -28,7 +28,7 @@ from django.conf import settings
 DEBUG = False
 
 DATABASES = settings.DATABASES
-import dj_database_url
+
 DATABASES['default'] = dj_database_url.config()
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
