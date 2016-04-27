2a9c2f9db951d39c7e5f256e09d088e2a66e0faa
sudharsan78 <sudharsan@testpress.in>
2016-04-27 19:28:34 +0530

    Modify settings

--- todo/settings/production.py
+++ todo/settings/production.py
@@ -29,9 +29,7 @@ DEBUG = False
 
 DATABASES = settings.DATABASES
 
-DATABASES['default'] = dj_database_url.config()
 
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 ALLOWED_HOSTS = []
 
