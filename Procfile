7fa3ea0dc365dc700faaa8efc82ace6b1984c59d
sudharsan78 <sudharsan@testpress.in>
2016-04-28 10:45:56 +0530

    Modify settings

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
-
 web: gunicorn todo.wsgi --log-file -
--- todo/wsgi.py
+++ todo/wsgi.py
@@ -8,19 +8,15 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
 import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "todo.settings")
 
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "todo.settings")
+
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
 
-try:
-	from dj_static import Cling
 
-	application = Cling(get_wsgi_application())
-except:
-	pass
 	
