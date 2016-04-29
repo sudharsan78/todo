fc7354969607c4ada1d314f4568d41eac0269c90
sudharsan78 <sudharsan@testpress.in>
2016-04-29 14:50:13 +0530

    Add toggle for completed

--- api/urls.py
+++ api/urls.py
@@ -7,7 +7,7 @@ from django.views.generic import TemplateView
 app_name='api'
 urlpatterns=[
     url(r'^$', views.TodoAPIView.as_view()),
-    url(r'^(?P<pk>\d+)/$', views.TodoDetail.as_view()),
+    url(r'^(?P<pk>\d+)$', views.TodoDetail.as_view()),
       
 ]
 urlpatterns = format_suffix_patterns(urlpatterns)
